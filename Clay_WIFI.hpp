97c73e9af2b2b6267224442c94727a35a53025ca
ClayNeko <157787755+ClayNeko@users.noreply.github.com>
2024-05-01 19:07:00 +0800

    Update Clay_WIFI.hpp

--- Clay_WIFI.hpp
+++ Clay_WIFI.hpp
@@ -1,13 +1,11 @@
 #pragma once
+
 #include <string.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/event_groups.h"
 #include "esp_wifi.h"
-#include "esp_log.h"
 #include "esp_event.h"
-#include "nvs_flash.h"
 
 #define DEFAULT_SCAN_LIST_SIZE 10
+
 
 struct wifi_scan_result {
     wifi_ap_record_t ap_info[DEFAULT_SCAN_LIST_SIZE];
@@ -17,90 +15,12 @@ struct wifi_scan_result {
 class Clay_WIFI
 {
 public:
-    char* get_authmode(int authmode);
-    char* get_pairwise_cipher(int pairwise_cipher);
-    char* get_group_cipher(int group_cipher);
     void init(wifi_mode_t wifi_mode);
     wifi_scan_result scan();
+    esp_err_t sta_connect(char* ssid, char* password, wifi_auth_mode_t authmode);
+    void ap_config(char* ssid, char* password, wifi_auth_mode_t authmode, uint8_t max_connection, uint8_t channel);
+    esp_netif_ip_info_t get_ip_info();
 };
-
-char* 
-Clay_WIFI::get_authmode(int authmode) {
-    switch (authmode) {
-    case WIFI_AUTH_OPEN:
-        return "WIFI_AUTH_OPEN";
-    case WIFI_AUTH_OWE:
-        return "WIFI_AUTH_OWE";
-    case WIFI_AUTH_WEP:
-        return "WIFI_AUTH_WEP";
-    case WIFI_AUTH_WPA_PSK:
-        return "WIFI_AUTH_WPA_PSK";
-    case WIFI_AUTH_WPA2_PSK:
-        return "WIFI_AUTH_WPA2_PSK";
-    case WIFI_AUTH_WPA_WPA2_PSK:
-        return "WIFI_AUTH_WPA_WPA2_PSK";
-    case WIFI_AUTH_WPA2_ENTERPRISE:
-        return "WIFI_AUTH_WPA2_ENTERPRISE";
-    case WIFI_AUTH_WPA3_PSK:
-        return "WIFI_AUTH_WPA3_PSK";
-    case WIFI_AUTH_WPA2_WPA3_PSK:
-        return "WIFI_AUTH_WPA2_WPA3_PSK";
-    default:
-        return "WIFI_AUTH_UNKNOWN";
-    }
-}
-char* 
-Clay_WIFI::get_pairwise_cipher(int pairwise_cipher) {
-    switch (pairwise_cipher) {
-    case WIFI_CIPHER_TYPE_NONE:
-        return "WIFI_CIPHER_TYPE_NONE";
-    case WIFI_CIPHER_TYPE_WEP40:
-        return "WIFI_CIPHER_TYPE_WEP40";
-    case WIFI_CIPHER_TYPE_WEP104:
-        return "WIFI_CIPHER_TYPE_WEP104";
-    case WIFI_CIPHER_TYPE_TKIP:
-        return "WIFI_CIPHER_TYPE_TKIP";
-    case WIFI_CIPHER_TYPE_CCMP:
-        return "WIFI_CIPHER_TYPE_CCMP";
-    case WIFI_CIPHER_TYPE_TKIP_CCMP:
-        return "WIFI_CIPHER_TYPE_TKIP_CCMP";
-    case WIFI_CIPHER_TYPE_AES_CMAC128:
-        return "WIFI_CIPHER_TYPE_AES_CMAC128";
-    case WIFI_CIPHER_TYPE_SMS4:
-        return "WIFI_CIPHER_TYPE_SMS4";
-    case WIFI_CIPHER_TYPE_GCMP:
-        return "WIFI_CIPHER_TYPE_GCMP";
-    case WIFI_CIPHER_TYPE_GCMP256:
-        return "WIFI_CIPHER_TYPE_GCMP256";
-    default:
-        return "WIFI_CIPHER_TYPE_UNKNOWN";
-    }
-}
-char* 
-Clay_WIFI::get_group_cipher(int group_cipher) {
-    switch (group_cipher) {
-    case WIFI_CIPHER_TYPE_NONE:
-        return "WIFI_CIPHER_TYPE_NONE";
-    case WIFI_CIPHER_TYPE_WEP40:
-        return "WIFI_CIPHER_TYPE_WEP40";
-    case WIFI_CIPHER_TYPE_WEP104:
-        return "WIFI_CIPHER_TYPE_WEP104";
-    case WIFI_CIPHER_TYPE_TKIP:
-        return "WIFI_CIPHER_TYPE_TKIP";
-    case WIFI_CIPHER_TYPE_CCMP:
-        return "WIFI_CIPHER_TYPE_CCMP";
-    case WIFI_CIPHER_TYPE_TKIP_CCMP:
-        return "WIFI_CIPHER_TYPE_TKIP_CCMP";
-    case WIFI_CIPHER_TYPE_SMS4:
-        return "WIFI_CIPHER_TYPE_SMS4";
-    case WIFI_CIPHER_TYPE_GCMP:
-        return "WIFI_CIPHER_TYPE_GCMP";
-    case WIFI_CIPHER_TYPE_GCMP256:
-        return "WIFI_CIPHER_TYPE_GCMP256";
-    default:
-        return "WIFI_CIPHER_TYPE_UNKNOWN";
-    }
-}
 
 void
 Clay_WIFI::init(wifi_mode_t wifi_mode) {
@@ -127,5 +47,59 @@ Clay_WIFI::scan() {
     esp_wifi_scan_start(NULL, true);
     ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(&number, scan_result.ap_info));
     ESP_ERROR_CHECK(esp_wifi_scan_get_ap_num(&scan_result.ap_count));
+
     return scan_result;
+}
+
+esp_err_t
+Clay_WIFI::sta_connect(
+    char* ssid, 
+    char* password, 
+    wifi_auth_mode_t authmode = WIFI_AUTH_WPA_WPA2_PSK
+) {
+    wifi_config_t wifi_config;
+    ESP_ERROR_CHECK(esp_wifi_get_config(WIFI_IF_STA, &wifi_config));
+
+    strcpy((char *) wifi_config.sta.ssid, ssid);
+    strcpy((char *) wifi_config.sta.password, password);
+    wifi_config.sta.threshold.authmode = authmode;
+
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+
+    esp_err_t connect_error = esp_wifi_connect();
+
+    return connect_error;
+}
+
+void 
+Clay_WIFI::ap_config(
+    char* ssid, 
+    char* password, 
+    wifi_auth_mode_t authmode = WIFI_AUTH_WPA_WPA2_PSK, 
+    uint8_t max_connection = 10, 
+    uint8_t channel = 6
+) {
+    wifi_config_t wifi_config_sta, wifi_config_ap;
+    ESP_ERROR_CHECK(esp_wifi_get_config(WIFI_IF_STA, &wifi_config_sta));
+    ESP_ERROR_CHECK(esp_wifi_get_config(WIFI_IF_AP, &wifi_config_ap));
+    ESP_ERROR_CHECK(esp_wifi_stop());
+
+    strcpy((char *) wifi_config_ap.ap.ssid, ssid);
+    wifi_config_ap.ap.ssid_len = strlen(ssid);
+    wifi_config_ap.ap.channel = channel;
+    strcpy((char *) wifi_config_ap.ap.password, password);
+    wifi_config_ap.ap.authmode = authmode;
+    
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config_sta));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config_ap));
+
+    ESP_ERROR_CHECK(esp_wifi_start());
+}
+
+esp_netif_ip_info_t
+Clay_WIFI::get_ip_info() {
+    esp_netif_ip_info_t ip_info;
+    esp_netif_get_ip_info(esp_netif_get_handle_from_ifkey("WIFI_AP_DEF"), &ip_info);
+
+    return ip_info;
 }
