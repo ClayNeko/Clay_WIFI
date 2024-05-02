ce3df7d37a947720537299c9d16701db38d2891c
ClayNeko <157787755+ClayNeko@users.noreply.github.com>
2024-05-02 15:20:12 +0800

    Update Clay_WIFI.hpp

--- Clay_WIFI.hpp
+++ Clay_WIFI.hpp
@@ -6,7 +6,7 @@
 
 #define DEFAULT_SCAN_LIST_SIZE 10
 
-static const char *Clay_TAG = "Clay_WIFI";
+static const char *Clay_WIFI_TAG = "Clay_WIFI";
 
 typedef struct {
     wifi_ap_record_t    ap_info[DEFAULT_SCAN_LIST_SIZE];
@@ -17,6 +17,7 @@ typedef struct {
     char                ssid[32];
     char                password[64];
     wifi_auth_mode_t    authmode = WIFI_AUTH_WPA_WPA2_PSK;
+    char                mac[18] = { 0 };
 } clay_wifi_config_sta;
 
 typedef struct {
@@ -28,6 +29,7 @@ typedef struct {
 } clay_wifi_config_ap;
 
 typedef struct {
+    wifi_mode_t wifi_mode;
     clay_wifi_config_sta sta;
     clay_wifi_config_ap ap;
 } clay_wifi_config;
@@ -35,15 +37,17 @@ typedef struct {
 class Clay_WIFI
 {
 public:
-    void init(wifi_mode_t wifi_mode, clay_wifi_config clay_cfg);
+    void init(clay_wifi_config clay_cfg);
     wifi_scan_result scan();
-    esp_err_t sta_connect(char* ssid, char* password, wifi_auth_mode_t authmode);
-    void ap_config(char* ssid, char* password, wifi_auth_mode_t authmode, uint8_t max_connection, uint8_t channel);
+    esp_err_t connect();
+    esp_err_t connect(char* ssid, char* password, wifi_auth_mode_t authmode);
     esp_netif_ip_info_t get_ip_info();
+private:
+    void hex_to_mac(uint8_t* dest, char* src);
 };
 
 void
-Clay_WIFI::init(wifi_mode_t wifi_mode, clay_wifi_config clay_cfg) {
+Clay_WIFI::init(clay_wifi_config clay_cfg) {
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
@@ -54,10 +58,10 @@ Clay_WIFI::init(wifi_mode_t wifi_mode, clay_wifi_config clay_cfg) {
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
-    ESP_ERROR_CHECK(esp_wifi_set_mode(wifi_mode));
+    ESP_ERROR_CHECK(esp_wifi_set_mode(clay_cfg.wifi_mode));
 
-    if (wifi_mode == WIFI_MODE_STA || wifi_mode == WIFI_MODE_APSTA) {
-        ESP_LOGI(Clay_TAG, "wifi_mode includes WIFI_MODE_STA");
+    if (clay_cfg.wifi_mode == WIFI_MODE_STA || clay_cfg.wifi_mode == WIFI_MODE_APSTA) {
+        ESP_LOGI(Clay_WIFI_TAG, "wifi_mode includes WIFI_MODE_STA");
         wifi_config_t wifi_config_sta = {
             .sta = {
                 .ssid = { (uint8_t) atoi(clay_cfg.sta.ssid) },
@@ -69,16 +73,24 @@ Clay_WIFI::init(wifi_mode_t wifi_mode, clay_wifi_config clay_cfg) {
         };
         strcpy((char *) wifi_config_sta.sta.ssid, clay_cfg.sta.ssid);
         strcpy((char *) wifi_config_sta.sta.password, clay_cfg.sta.password);
+        if (strlen(clay_cfg.sta.mac) != 0) {
+            uint8_t MACAddress[6] = { 0 };
+            hex_to_mac(MACAddress, clay_cfg.sta.mac);
+
+            ESP_ERROR_CHECK(esp_wifi_set_mac(WIFI_IF_STA, &MACAddress[0]));
+            memcpy(wifi_config_sta.sta.bssid,MACAddress,sizeof(MACAddress));
+        }
+        
 
         ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config_sta));
 
-        ESP_LOGI(Clay_TAG, "wifi_config_sta ->");
-        ESP_LOGI(Clay_TAG, "\t ssid - %s", (char*) &wifi_config_sta.sta.ssid);
-        ESP_LOGI(Clay_TAG, "\t pass - %s", (char*) &wifi_config_sta.sta.password);
+        ESP_LOGI(Clay_WIFI_TAG, "wifi_config_sta ->");
+        ESP_LOGI(Clay_WIFI_TAG, "\t ssid - %s", (char*) &wifi_config_sta.sta.ssid);
+        ESP_LOGI(Clay_WIFI_TAG, "\t pass - %s", (char*) &wifi_config_sta.sta.password);
     }
     
-    if (wifi_mode == WIFI_MODE_AP || wifi_mode == WIFI_MODE_APSTA) {
-        ESP_LOGI(Clay_TAG, "wifi_mode includes WIFI_MODE_AP");
+    if (clay_cfg.wifi_mode == WIFI_MODE_AP || clay_cfg.wifi_mode == WIFI_MODE_APSTA) {
+        ESP_LOGI(Clay_WIFI_TAG, "wifi_mode includes WIFI_MODE_AP");
         wifi_config_t wifi_config_ap = {
             .ap = {
                 .ssid = { (uint8_t) atoi(clay_cfg.ap.ssid) },
@@ -98,13 +110,12 @@ Clay_WIFI::init(wifi_mode_t wifi_mode, clay_wifi_config clay_cfg) {
 
         ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP,  &wifi_config_ap));
 
-        ESP_LOGI(Clay_TAG, "wifi_config_ap ->");
-        ESP_LOGI(Clay_TAG, "\t ssid - %s", (char*) &wifi_config_ap.ap.ssid);
-        ESP_LOGI(Clay_TAG, "\t pass - %s", (char*) &wifi_config_ap.ap.password);
+        ESP_LOGI(Clay_WIFI_TAG, "wifi_config_ap ->");
+        ESP_LOGI(Clay_WIFI_TAG, "\t ssid - %s", (char*) &wifi_config_ap.ap.ssid);
+        ESP_LOGI(Clay_WIFI_TAG, "\t pass - %s", (char*) &wifi_config_ap.ap.password);
     }
 
     ESP_ERROR_CHECK(esp_wifi_start());
-    ESP_ERROR_CHECK(esp_wifi_connect());
 }
 
 wifi_scan_result
@@ -123,7 +134,13 @@ Clay_WIFI::scan() {
 }
 
 esp_err_t
-Clay_WIFI::sta_connect(
+Clay_WIFI::connect() {
+    esp_err_t connect_error = esp_wifi_connect();
+    return connect_error;
+}
+
+esp_err_t
+Clay_WIFI::connect(
     char* ssid, 
     char* password, 
     wifi_auth_mode_t authmode = WIFI_AUTH_WPA_WPA2_PSK
@@ -138,7 +155,6 @@ Clay_WIFI::sta_connect(
     ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
 
     esp_err_t connect_error = esp_wifi_connect();
-
     return connect_error;
 }
 
@@ -148,4 +164,18 @@ Clay_WIFI::get_ip_info() {
     esp_netif_get_ip_info(esp_netif_get_handle_from_ifkey("WIFI_STA_DEF"), &ip_info);
 
     return ip_info;
+}
+
+void
+Clay_WIFI::hex_to_mac(uint8_t* dest, char* src) {
+    int values[6]; 
+    int i; 
+
+    if(6 == sscanf(src, "%x:%x:%x:%x:%x:%x%*c", 
+        &values[0], &values[1], &values[2], 
+        &values[3], &values[4], &values[5])) 
+    { 
+        for(i = 0; i < 6; ++i) 
+            dest[i] = (uint8_t) values[i]; 
+    }
 }
